81add3f0619e9e5dbc7e89d85e4202b8b3b32dd5
Nyshiyama <9534sdkmmmk@gmail.com>
2020-04-01 01:13:33 +0900

    「論理型」サンプル

--- sample.f90
+++ sample.f90
@@ -8,9 +8,7 @@ program main
     double precision, dimension(nx,ny) :: u, u_new
     integer i, j
     integer xp, xm, yp, ym
-
-    ! 初期値
-    u(:,:)=1d0
+    logical, dimension(nx,ny) :: obs
 
     delta_x=xmax/dble(nx-1)
     delta_y=ymax/dble(ny-1)
@@ -19,19 +17,35 @@ program main
     ry=delta_t/delta_y**2 ! 陽解法式参照
     print*, 'rx=',rx,'ry=',ry ! rx=   9.0000000000000011E-002 ry=   3.9999999999999994E-002
 
+    ! 障害物の設定
+    obs(:,:)=.true.
+    do j=2,ny-1
+        do i=2,nx-1
+            obs(i,j)=.false.
+        enddo
+    enddo
+
+    ! uの初期条件
     do j=1,ny
         do i=1,nx
-            xp=i+1
-            if(i==nx) xp=1
-            xm=i-1
-            if(i==1) xm=nx
-            yp=j+1
-            if(j==ny) yp=1
-            ym=j-1
-            if(j==1) ym=ny
-            ! if文を使わずにxp等は記述できる（ヒント：mod関数，答えは最初のサンプルコード）
+            if(obs(i,j))then
+                u(i,j)=0d0
+            else
+                u(i,j)=1d0
+            endif
+        enddo
+    enddo
 
-            u_new(i,j)=rx*(u(xp,j)+u(xm,j))+ry*(u(i,yp)+u(i,ym))+(1d0-2d0*(rx+ry))*u(i,j) ! 陽解法式参照
+    do j=1,ny
+        do i=1,nx
+            if(.not. obs(i,j))then
+                xp=mod(i,nx)+1
+                xm=nx-mod(nx+1-i,nx)
+                yp=mod(j,ny)+1
+                ym=ny-mod(ny+1-j,ny)
+
+                u_new(i,j)=rx*(u(xp,j)+u(xm,j))+ry*(u(i,yp)+u(i,ym))+(1d0-2d0*(rx+ry))*u(i,j) ! 陽解法式参照
+            endif
         enddo
     enddo
 
