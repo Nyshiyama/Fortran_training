bc88138ea6b74eb1c4c9c68b9652647b8fdb381e
Nyshiyama <9534sdkmmmk@gmail.com>
2020-03-31 22:44:51 +0900

    「デバッグ練習用バグコードの修正」サンプル

--- shisokubug_ex.f90
+++ shisokubug_ex.f90
@@ -1,6 +1,8 @@
 program main
-double precision delta_x, delta_y ! 格子幅
+    implicit none ! ←追加
+    double precision delta_x, delta_y ! 格子幅
     double precision delta_t  ! 時間刻み
+    double precision kx, ky ! ←追加
     double precision u, u_new
     double precision uxp, uxm, uyp, uym
 
@@ -10,17 +12,18 @@ double precision delta_x, delta_y ! 格子幅
   delta_t=1d-4
 
     ! 初期値
+    u  =1d0 ! ←追加
     uxp=2d0
     uxm=2d0
-                        uyp=2d0
+    uyp=2d0
     uym=2d0
 
     kx=delta_t/delta_x**2 ! 陽解法式参照
-    ky=     delta_t/delta_y**2 ! 陽解法式参照
+    ky=delta_t/delta_y**2 ! 陽解法式参照
 
     print*, 'kx, ky', kx, ky
 
-u_new=kx*(uxp+uxm)+ky*(uyp+uym)+(1d0-2d0*(kx+ky))*u ! 陽解法式参照
+    u_new=kx*(uxp+uxm)+ky*(uyp+uym)+(1d0-2d0*(kx+ky))*u ! 陽解法式参照
 
 print*, 'u_new', u_new ! 本来は u_new   1.0072222222222222
 
