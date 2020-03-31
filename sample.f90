8c2e863379f77358828828e9a478031b98a753e7
Nyshiyama <9534sdkmmmk@gmail.com>
2020-04-01 08:40:19 +0900

    「ParaView確認」サンプル

--- sample.f90
+++ sample.f90
@@ -6,9 +6,11 @@ program main
     double precision delta_x, delta_y ! 格子幅
     double precision rx, ry
     double precision, dimension(nx,ny) :: u, u_new
+    logical         , dimension(nx,ny) :: obs
+    integer         , dimension(nx,ny) :: Iobs ! ParaView
     integer i, j
     integer xp, xm, yp, ym
-    logical, dimension(nx,ny) :: obs
+    character cnx*8, cny*8, cnz*8, cntot*16, cdx*12, cdy*12, buffer*80 ! ParaView
 
     delta_x=xmax/dble(nx-1)
     delta_y=ymax/dble(ny-1)
@@ -24,6 +26,39 @@ program main
             obs(i,j)=.false.
         enddo
     enddo
+
+    ! 障害物データを整数型に変換
+    do j=1,ny
+        do i=1,nx
+            if(obs(i,j))then
+                Iobs(i,j)=1
+            else
+                Iobs(i,j)=0
+            endif
+        enddo
+    enddo
+
+    write(cnx,'(i8)') nx
+    write(cny,'(i8)') ny
+    write(cnz,'(i8)') 1
+    write(cntot,'(i16)') nx*ny
+    write(cdx,'(f12.10)') delta_x
+    write(cdy,'(f12.10)') delta_y
+
+    ! ParaViewフォーマット
+    open(20, file='obs.vtk', form='unformatted', access='stream', status='unknown', convert='BIG_ENDIAN')
+    buffer = '# vtk DataFile Version 3.0'//char(10) ; write(20) trim(buffer)
+    buffer = 'contour.vtk'//char(10)                ; write(20) trim(buffer)
+    buffer = 'BINARY'//char(10)                     ; write(20) trim(buffer)
+    buffer = 'DATASET STRUCTURED_POINTS'//char(10)  ; write(20) trim(buffer)
+    buffer = 'DIMENSIONS '//cnx//cny//cnz//char(10) ; write(20) trim(buffer)
+    buffer = 'ORIGIN 0.0 0.0 0.0'//char(10)         ; write(20) trim(buffer)
+    buffer = 'SPACING '//cdx//cdy//' 1.0'//char(10) ; write(20) trim(buffer)
+    buffer = 'POINT_DATA'//cntot//char(10)          ; write(20) trim(buffer)
+    buffer = 'SCALARS obs int'//char(10)            ; write(20) trim(buffer)
+    buffer = 'LOOKUP_TABLE default'//char(10)       ; write(20) trim(buffer)
+    write(20) ((Iobs(i,j),i=1,nx),j=1,ny)
+    close(20)
 
     ! uの初期条件
     do j=1,ny
@@ -49,9 +84,19 @@ program main
         enddo
     enddo
 
-    open(10,file='open_ex.txt',form='formatted',status='unknown')
-    write(10,*) nx, ny, 'u_new' ! ヘッダー：ファイル冒頭に格子点数等を記録しておくと参照するときに便利
-    write(10,*) u_new(:,:) ! 先頭の文字列は参照するときに邪魔なので削除（printのときは出力値を明確化するために付けていた）
-    close(10)
+    ! ParaViewフォーマット
+    open(20, file='u.vtk', form='unformatted', access='stream', status='unknown', convert='BIG_ENDIAN')
+    buffer = '# vtk DataFile Version 3.0'//char(10) ; write(20) trim(buffer)
+    buffer = 'contour.vtk'//char(10)                ; write(20) trim(buffer)
+    buffer = 'BINARY'//char(10)                     ; write(20) trim(buffer)
+    buffer = 'DATASET STRUCTURED_POINTS'//char(10)  ; write(20) trim(buffer)
+    buffer = 'DIMENSIONS '//cnx//cny//cnz//char(10) ; write(20) trim(buffer)
+    buffer = 'ORIGIN 0.0 0.0 0.0'//char(10)         ; write(20) trim(buffer)
+    buffer = 'SPACING '//cdx//cdy//' 1.0'//char(10) ; write(20) trim(buffer)
+    buffer = 'POINT_DATA'//cntot//char(10)          ; write(20) trim(buffer)
+    buffer = 'SCALARS u float'//char(10)            ; write(20) trim(buffer)
+    buffer = 'LOOKUP_TABLE default'//char(10)       ; write(20) trim(buffer)
+    write(20) ((real(u_new(i,j)),i=1,nx),j=1,ny)
+    close(20)
 
 end program main
